6c62996b14915c8e2bfeaf62cac40f9c2485261a
Chris Brown <cj_browna@hotmail.com>
2019-01-07 23:39:36 +0000

    adds answer button click

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+const NumberButton = ({ number, onClick }) => (
+  <View style={styles.button}>
+    <Button
+      onPress={() => onClick(number)}
+      title={number.toString()}
+    />
+  </View>
+);
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { answer: '?' };
+    this._onSelect = this._onSelect.bind(this);
+  }
+
+  _onSelect(answer) {
+    this.setState({ answer });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={{ ...styles.row, flex: 4, backgroundColor: 'powderblue' }}>
-          <Text style={styles.button}>1</Text>
-          <Text style={styles.button}>2</Text>
-          <Text style={styles.button}>3</Text>
-          <Text style={styles.button}>4</Text>
+          <View style={styles.buttonWrapper}>
+            {
+              [1,2,3,4,5,6,7,8,9,10].map(number =>
+                <NumberButton number={number} key={number} onClick={this._onSelect} />
+              )
+            }
+          </View>
         </View>
         <View style={{ ...styles.row, flex: 1, backgroundColor: 'skyblue' }}>
-          <Text>Yo yo</Text>
+          <Text style={{ fontSize: 20 }}>5 + 2 = { this.state.answer }</Text>
         </View>
       </View>
     );
@@ -23,17 +47,21 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100%'
+    minHeight: '100%',
+    fontSize: 24,
   },
   row: {
     justifyContent: 'flex-start',
     flexDirection: 'row',
     alignItems: 'center',
   },
+  buttonWrapper: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+  },
   button: {
     width: 50,
     height: 50,
-    backgroundColor: 'red',
     color: 'white',
     margin: 5,
   }
